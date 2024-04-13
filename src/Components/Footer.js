05243191e4f0fdd1426a04432aa5ecd59198997d
Kambale Dessai <93641789+kdessaik@users.noreply.github.com>
2024-04-14 00:12:08 +0300

    allchange

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,55 +8,29 @@ export default function Footer() {
   return (
     
 <>
-<footer >
+<footer className='footer' >
   
-  <div className="container p-4">
+  <div className="container ">
 
     
     
 
 
      
-    <section className="bg-light text-center">
-      <form action="">
-              
-        <div className="row d-flex justify-content-center">
-    
-          <div className="col-auto">
-            <p className="pt-2">
-              <strong>Sign up for our newsletter</strong>
-            </p>
-          </div>
-    
+    <section className="contactFooter">
+        <h5 >CONTACT US</h5>
+        <a>info@learninglions.org</a>
+        <address>Kenya – P.O. Box 344 – 30500 Lodwar</address>
+        <address>Germany – Schlossstraße 8, 82269 Geltendorf</address>
 
-    
-          <div className="col-md-5 col-12">
-          
-            <div className="form-outline mb-4">
-              <input type="email" id="form5Example2" className="form-control" />
-              <label className="form-label" for="form5Example2">Email address</label>
-            </div>
-          </div>
-    
-
-    
-          <div className="col-auto">
-
-          
-            <button type="submit" className="btn btn-primary mb-4">
-              Subscribe
-            </button>
-          </div>
-    
-        </div>
-              
-      </form>
+      
     </section>
      
 
 
     
-    <section className="mb-4">
+    <section className="contactFooter">
+    <h5 >FOLLOW US</h5>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
         distinctio earum repellat quaerat voluptatibus placeat nam,
@@ -72,24 +46,7 @@ export default function Footer() {
             
       <div className="row">
   
-        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase">Links</h5>
-
-          <ul className="list-unstyled mb-0">
-            <li>
-              <a href="#!" className="text-dark">Link 1</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 2</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 3</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 4</a>
-            </li>
-          </ul>
-        </div>
+       
   
 
   
@@ -162,10 +119,6 @@ export default function Footer() {
   </div>
 
  
-  <div className="text-center p-3" style={{backgroundColor: 'blue'}}>
-    © 2020 Copyright:
-    <a className="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
  
 
 </footer>
