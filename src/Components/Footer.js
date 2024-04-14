75b7aabe6828aa1de89122e0ff0b9d46de72d615
Kambale Dessai <93641789+kdessaik@users.noreply.github.com>
2024-04-14 15:39:18 +0300

    all the changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,12 +3,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../style/css/Footer.css'
+import fIcon from '../style/images/scialMediIcon/1.png'
+import iIcon from '../style/images/scialMediIcon/222.png'
+import XIcon from '../style/images/scialMediIcon/3.png'
+import YIcon from '../style/images/scialMediIcon/4.png'
+import LIcon from '../style/images/scialMediIcon/5.png'
+
+
 
 export default function Footer() {
   return (
     
 <>
 <footer className='footer' >
+  <section className='footerEmail'>
+    <form> <h6>SUBSCRIBE TO OUR NEWSLETTER</h6>
+    <input placeholder='Your@email.com'/>
+    <button>SUBSCRIBE</button>
+    </form>
+   
+
+  </section>
   
   <div className="container ">
 
@@ -31,18 +46,31 @@ export default function Footer() {
     
     <section className="contactFooter">
     <h5 >FOLLOW US</h5>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-        distinctio earum repellat quaerat voluptatibus placeat nam,
-        commodi optio pariatur est quia magnam eum harum corrupti dicta,
-        aliquam sequi voluptate quas.
-      </p>
+      <div className='socialMedia'>
+      <i className="bi bi-socialMedia"><a target="_blank" href='https://www.facebook.com/learninglionskenya/?_rdc=2&_rdr'>
+        <img src={fIcon}/>
+        </a> </i>
+        <i className="bi bi-socialMedia"><a target="_blank" href='https://www.instagram.com/learning_lions/'>
+        <img className='iconSameSize' src={iIcon}/>
+        </a> </i>
+        <i className="bi bi-socialMedia"><a target="_blank" href='https://twitter.com/learning_lions'>
+        <img className='iconSameSize' src={XIcon}/>
+        </a> </i>
+        <i className="bi bi-socialMedia"><a target="_blank" href='https://www.youtube.com/channel/UCtdMZg2Dh6kwyyqgqqwrX0Q'>
+        <img className='iconSameSize' src={YIcon}/>
+        </a> </i>
+        <i className="bi bi-socialMedia"><a target="_blank" href='https://www.linkedin.com/company/startuplions?originalSubdomain=ke'>
+        <img src={LIcon}/>
+        </a> </i>
+        
+      </div>
+      
     </section>
     
 
 
       
-    <section className="">
+    <section >
             
       <div className="row">
   
@@ -51,72 +79,43 @@ export default function Footer() {
 
   
         <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase">Links</h5>
+         <br/>
 
           <ul className="list-unstyled mb-0">
-            <li>
-              <a href="#!" className="text-dark">Link 1</a>
+            <li className='Policy'>
+              <a href="#!" className="text-white1">imprint</a>
             </li>
-            <li>
-              <a href="#!" className="text-dark">Link 2</a>
+            <li className='Policy'>
+              <a href="#!" className="text-white1">privacy</a>
             </li>
-            <li>
-              <a href="#!" className="text-dark">Link 3</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 4</a>
-            </li>
+           
           </ul>
         </div>
   
 
-  
-        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase">Links</h5>
-
-          <ul className="list-unstyled mb-0">
-            <li>
-              <a href="#!" className="text-dark">Link 1</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 2</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 3</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 4</a>
-            </li>
-          </ul>
-        </div>
-  
+     
 
   
-        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase">Links</h5>
-
-          <ul className="list-unstyled mb-0">
-            <li>
-              <a href="#!" className="text-dark">Link 1</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 2</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 3</a>
-            </li>
-            <li>
-              <a href="#!" className="text-dark">Link 4</a>
-            </li>
-          </ul>
-        </div>
+       
   
       </div>
-            
+
+     
     </section>
       
 
   </div>
+  <section className='footerDescription'>
+    <p>
+    Learning Lions is the first step of a youth empowering program preceding
+
+    </p>
+   <span>
+   <a href=''>www.digitallions.co </a> 
+    <a href=''>and www.startuplions.org</a>
+    </span> 
+  </section>
+  
 
  
  
